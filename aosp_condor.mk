a2b6b7f41d79b0c45bff7dc1b132385830ef70ed
INSPIRE-REX <inspirerex@gmail.com>
2016-10-15 18:47:34 +0530

    correct path

--- aosp_condor.mk
+++ aosp_condor.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosp_condor
